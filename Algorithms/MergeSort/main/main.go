9bb8010ac21030ede164b40aa3291b9e980e724e
白泽琛 <baizechen@xiaoniangao.com>
2023-08-09 16:29:58 +0800

    归并排序--第三版 增加迭代的方式进行排序

--- Algorithms/MergeSort/main/main.go
+++ Algorithms/MergeSort/main/main.go
@@ -25,7 +25,9 @@ func (m *MergeSort[T]) Sort() []T {
 	// 为了避免每次merge时都开辟空间，这里直接先定义好
 	// 这里做了一下内存开辟的优化
 	copyArr := make([]T, len(m.arr))
-	m.sort(0, len(m.arr)-1, copyArr)
+	m.sortRecursion(0, len(m.arr)-1, copyArr)
+	// 非递归
+	//m.sortIteration(copyArr)
 	return m.arr
 }
 
@@ -51,18 +53,39 @@ func (m *MergeSort[T]) merge(l, middle, r int, copyArr []T) {
 	}
 }
 
-func (m *MergeSort[T]) sort(l, r int, copyArr []T) {
+// 递归的方式，自顶向下
+func (m *MergeSort[T]) sortRecursion(l, r int, copyArr []T) {
 	if l >= r {
 		return
 	}
 
 	middle := (l + r) / 2
-	m.sort(l, middle, copyArr)
-	m.sort(middle+1, r, copyArr)
+	m.sortRecursion(l, middle, copyArr)
+	m.sortRecursion(middle+1, r, copyArr)
 
 	// 这里做一下优化，当middle的值已经小于右边最小的第一个值后，
 	// 就不需要merge了
 	if m.arr[middle] > m.arr[middle+1] {
 		m.merge(l, middle, r, copyArr)
+	}
+}
+
+// 迭代的方式，自底向上
+func (m *MergeSort[T]) sortIteration(copyArr []T) {
+	length := len(m.arr)
+	for step := 1; step < length; step += step {
+		// 这里合并的数组边界为 [i,i+step-1] [i+step, i+step+step-1]
+		// 所以，l=i middle = i+step-1 r = i+step+step-1
+		// 当然 i+step+step-1有可能越界，所以正确的r= min(n-1, i+step+step-1)
+		for i := 0; i+step < length; i += step + step {
+			// 当然这里也加一下优化：当middle的值小于middle+1的值，就不再进行merge了
+			if m.arr[i+step-1] > m.arr[i+step] {
+				if i+step+step-1 < length {
+					m.merge(i, i+step-1, i+step+step-1, copyArr)
+				} else {
+					m.merge(i, i+step-1, length-1, copyArr)
+				}
+			}
+		}
 	}
 }
