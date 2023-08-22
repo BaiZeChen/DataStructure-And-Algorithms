1563264cac708c8bcca74e5dc2e36b1e193bac88
白泽琛 <baizechen@xiaoniangao.com>
2023-08-22 18:11:33 +0800

    快排v4版本 -- 三路快排

--- Algorithms/FastSort/main/main.go
+++ Algorithms/FastSort/main/main.go
@@ -9,10 +9,9 @@ import (
 // 快速排序：
 // 最坏：O(n^2) 概率非常低，平均：O(nlogn)
 func main() {
-	tmp := []int{6, 10, 1, 13, 8, 5, 2, 87, 23, 37}
-	sortV3(tmp, 0, len(tmp)-1)
+	tmp := []int{6, 10, 1, 13, 8, 5, 2, 87, 23, 37, 13, 13, 17, 23, 10, 9, 6, 6, 6, 1, 1, 1}
+	sortV4(tmp, 0, len(tmp)-1)
 	fmt.Println(tmp)
-
 }
 
 // 第一版快排版本
@@ -100,4 +99,39 @@ func partitionV3(arr []int, l, r int) int {
 	}
 	arr[l], arr[gte] = arr[gte], arr[l]
 	return gte
+}
+
+// 三路快排
+func sortV4(arr []int, l, r int) {
+	if l >= r {
+		return
+	}
+
+	lt, rt := partitionV4(arr, l, r)
+	sortV4(arr, l, lt)
+	sortV4(arr, rt, r)
+}
+func partitionV4(arr []int, l, r int) (int, int) {
+	result, _ := rand.Int(rand.Reader, big.NewInt(int64((r-l)+1)))
+	seed := int(int64(l) + result.Int64())
+	arr[seed], arr[l] = arr[l], arr[seed]
+
+	lt, index, gt := l, l+1, r+1
+	// 左边区间：arr[l+1:lt]< v 中间区间 arr[lt+1:gte-1]= v  右边区间：arr[gt:r] >v
+	for index < gt {
+		if arr[index] < arr[l] {
+			lt++
+			arr[lt], arr[index] = arr[index], arr[lt]
+			index++
+		} else if arr[index] == arr[l] {
+			index++
+		} else {
+			gt--
+			arr[index], arr[gt] = arr[gt], arr[index]
+		}
+	}
+	// 循环完后，将lt与l位置元素交换，此时区间为：
+	// 左边区间：arr[l:lt-1]< v 中间区间 arr[lt:gte-1]==v  右边区间：arr[gt:r] >v
+	arr[l], arr[lt] = arr[lt], arr[l]
+	return lt - 1, gt
 }
